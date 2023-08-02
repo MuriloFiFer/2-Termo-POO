3643c367431883d138796e3a978fc2469881d3ef
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-01 21:05:28 -0300

    01/08/23 - Aula Try Catch

--- Exception/ExemploTryCatch.java
+++ Exception/ExemploTryCatch.java
@@ -2,23 +2,29 @@ package Exception;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class ExemploTryCatch {
     
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        try {
-        System.out.print("Digite um valor inteiro..:");
-        int numero1 = s.nextInt();
-        System.out.print("Digite um valor inteiro..:");
-        int numero2 = s.nextInt();
-        System.out.println(numero1+ " + " + numero2 + "= " + (numero1+numero2));
-        } catch (Exception e) {
-        System.out.println("ERRO - Valor digitado nao e um numero inteiro!");
-    
+        boolean rodando = true;
+            while (rodando) {
+            try {
+                int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº Inteiro"));
+                int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº Inteiro"));
+                JOptionPane.showMessageDialog(null, "O resultado é "
+                        + (numero1 + numero2));
+                rodando = false;
+            } catch (NumberFormatException erro1) {
+                JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
+                                                                    +"\nDigite novamente");
+            } catch (Exception erro2) {
+                JOptionPane.showMessageDialog(null, "Aconteceu um erro"
+                                                                    +"\nDigite novamente");
+            }
         }
-        }
+    }
 }
-
 
 
