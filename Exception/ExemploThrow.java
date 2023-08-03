92f537b236dd59876e32b23725ede05c7b45aba8
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-03 20:31:17 -0300

    03/08/23 - Tratamento de exeptions

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -8,20 +8,32 @@ public class ExemploThrow {
         //digitação de senhas
        boolean testesenha = true;
        while (testesenha) {
-         String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos");  
+        String loginUser = JOptionPane.showInputDialog("Informe Seu Login:");
+         String dataNascimento = JOptionPane.showInputDialog("Informe sua data de nascimento [dd/mm/aa]:");  
+         String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos:");  
+         dataNascimento = dataNascimento.replace("/", ""); // Retirando   " / "  da data de nascimento para comparar com SENHA DIGITADA
               try{                
                 if (senhaDigitada.length()!=6) {
-                    throw new Exception("Senha Invalida");
+                    throw new Exception("Senha Invalida"); //menssagem se houver a excessão
                 }
-                testesenha = false;
-              } catch (Exception e) {
+                if (dataNascimento.equals(senhaDigitada)){
+                throw new Exception("Senha não pode ser igual a data de nascimento!");
+                }
+                  if (senhaDigitada.equals(loginUser)){     //se senha for igual login digitado      //equals para comparar String
+                 throw new Exception("Senha não pode ser igual Login");
+               
+                }
+                 testesenha = false;
+              }
+              catch (Exception e) {
                
                 
                 JOptionPane.showMessageDialog(null, e.getMessage());
               }
         }
     }
-}
+  }
+
 
 
                                                                     
