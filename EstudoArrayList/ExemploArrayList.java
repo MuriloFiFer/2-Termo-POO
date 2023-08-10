c82c2b00c8cb0a96a2246962a606546d7a201967
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-10 19:35:03 -0300

    10/08/23

--- EstudoArrayList/ExemploArrayList.java
+++ EstudoArrayList/ExemploArrayList.java
@@ -1,11 +1,15 @@
 package EstudoArrayList;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.JOptionPane;
 
 public class ExemploArrayList {
       //atributo
     //Declarando o ArrayList do Tipo String
     ArrayList<String> carros = new ArrayList<>();  //Nome do ArrayList "carros"
+    ArrayList<Integer> num = new ArrayList<>();
     //métodos
     public void teste() {
         //Adicionar Valores ao ArrayList
@@ -16,38 +20,34 @@ public class ExemploArrayList {
         carros.add("Gol");
         //Imprimir toda a Lista
         System.out.println(carros);
-        //Imprime posição 4 "Gol"
+        //Ordenando a Lista
+        Collections.sort(carros);
+        //pegando o valor correspondente a 5 posição(index 4)
         System.out.println(carros.get(4));
         //percorrendo a lista com FOR
-        for (int i = 0; i < carros.size(); i++) {  //percorrer ArrayList "size"
-        System.out.println(carros.get(i));           
+        for (int i = 0; i < carros.size(); i++) {
+            System.out.println(carros.get(i));
         }
+        //Collection.reverse(invertendo as listas)
+        Collections.reverse(carros);
         //percorrendo a lista com o FOR-EACH
         for (String i : carros) {
             System.out.println(i);
-            
         }
-
-
-        //apagar lista
+        //Apagar a Lista
         carros.clear();
         System.out.println(carros);
     }
-
     public void exercicio() {
         //criar um arraylist (Integer) + 5 valores
-        
-        ArrayList<String> numeros = new ArrayList<>();
-        
-        numeros.add("1");
-        numeros.add("2");
-        numeros.add("3");
-        numeros.add("4");
-        numeros.add("5");
-
-        System.out.println(numeros);
+        //preencher 5 nº int
+        for (int i = 0; i < 5; i++) {
+            num.add(Integer.parseInt(JOptionPane.showInputDialog("Digite um nº")));
+        }
+        for (int i : num) {
+            System.out.println(i);
+        }
+    }
+}
     
-        
-    }
-    }
 
