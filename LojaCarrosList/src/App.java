71e59da4b28a6ae0336ba07d6f9cc6dd82e7947f
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-10 21:56:47 -0300

    10/08/23 - Projeto Loja de Carros

---
+++ LojaCarrosList/src/App.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       // Criando Lista de Objetos
+       List <Carros> ListaCarros = new ArrayList<>();
+//1. Cadastrar Carro
+//2. Listar os Carros
+//3. Consultar Carro
+//4. Apagar Carro
+ JOptionPane.showInputDialog(null,"Informe o modelo do carro");
+ ListaCarros.add
+
+
+    }
+}
---
+++ LojaCarrosList/src/Carros.java
@@ -0,0 +1,44 @@
+public class Carros {
+    //Atributos
+    String marca;
+    String modelo;
+    String ano;
+    String cor;
+    //métodos
+    //Construtor(2 cheio, vazio)
+    public Carros(String marca, String modelo, String ano, String cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
+    public Carros() {
+    }
+    //getters abd setters
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getAno() {
+        return ano;
+    }
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
+
+}
