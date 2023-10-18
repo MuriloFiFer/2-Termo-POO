544685774f886932628bd5d7a02577c9408997c0
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-10-17 21:52:07 -0300

    FINAL PROJETO

--- SituacaoAprendizagem4/src/Task.java
+++ SituacaoAprendizagem4/src/Task.java
@@ -1,25 +1,29 @@
 public class Task {
-    //atributos
+    // atributos
     String description;
     boolean done;
-    //construtor
+
+    // construtor
     public Task(String description) {
         this.description = description;
         this.done = false;
     }
-    //gets and set
+    // gets and sets
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public boolean isDone() {
         return done;
     }
+
     public void setDone(boolean done) {
         this.done = done;
     }
-    
-    
+
 }
