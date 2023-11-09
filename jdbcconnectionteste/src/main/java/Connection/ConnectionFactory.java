af752c4cc62411c83d911c7ecb37c43665967f31
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-11-08 21:50:53 -0300

    08/11/23 projeto situaçãoAprendizagem 5

--- jdbcconnectionteste/src/main/java/Connection/ConnectionFactory.java
+++ jdbcconnectionteste/src/main/java/Connection/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     //atributos de conexão
-private static final String URL = "jdbc:postgresql://localhost:5432/postgres";  // "filnal", arquivo não pode ser alterado  "private"m arquivo independente.
+private static final String URL = "jdbc:postgresql://localhost:5432/postgres";  // "final", arquivo não pode ser alterado  "private"m arquivo independente.
 private static final String USUARIO = "postgres";
 private static final String SENHA = "postgres";
     //métodos de conexão
