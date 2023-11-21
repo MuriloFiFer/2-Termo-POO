0340ac54fe5f88f8e75bd0e2d3e1d86cdb2f5428
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-11-21 20:00:23 -0300

    21/11/23

--- SituacaoAprendizagem5/guijdbclojacarros/src/main/java/Connection/ClientesDAO.java
+++ SituacaoAprendizagem5/guijdbclojacarros/src/main/java/Connection/ClientesDAO.java
@@ -1,4 +1,4 @@
-package Connection;
+package main.java.Connection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,24 +8,28 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.Clientes;
+import main.java.Model.Clientes;
 
 public class ClientesDAO {
-    // atributo
+
+    // Atributos
     private Connection connection;
     private List<Clientes> clientes;
 
-    // construtor
+    // Construtor
     public ClientesDAO() {
         this.connection = ConnectionFactory.getConnection();
     }
 
+    // Métodos do CRUD
     // criar Tabela
     public void criaTabela() {
-        String sql = "CREATE TABLE IF NOT EXISTS clientes_lojaclientes (nome VARCHAR(255),MODELO VARCHAR(255),ANO VARCHAR(255),PLACA VARCHAR(255) PRIMARY KEY, VALOR VARCHAR(255))";
+        String sql = "CREATE TABLE IF NOT EXISTS clientes_lojacarros2 (NOME VARCHAR(255),IDADE VARCHAR(255),SEXO VARCHAR(255),RG VARCHAR(255),CPF VARCHAR(255) PRIMARY KEY)";
+
         try (Statement stmt = this.connection.createStatement()) {
             stmt.execute(sql);
             System.out.println("Tabela criada com sucesso.");
+
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao criar a tabela: " + e.getMessage(), e);
         } finally {
@@ -42,8 +46,7 @@ public class ClientesDAO {
         clientes = new ArrayList<>();
         // Cria uma lista para armazenar os clientes recuperados do banco de dados
         try {
-            String sql = "SELECT * FROM clientes_lojaclientes";
-            stmt = connection.prepareStatement(sql);
+            stmt = connection.prepareStatement("SELECT * FROM clientes_lojacarros2");
             // Prepara a consulta SQL para selecionar todos os registros da tabela
             rs = stmt.executeQuery();
             // Executa a consulta e armazena os resultados no ResultSet
@@ -51,13 +54,13 @@ public class ClientesDAO {
                 // Para cada registro no ResultSet, cria um objeto clientes com os valores do
                 // registro
 
-               Clientes cliente = newClientes(
+                Clientes cliente = new Clientes(
                         rs.getString("nome"),
-                        rs.getString(""),
-                        rs.getString("ano"),
-                        rs.getString("placa"),
-                        rs.getString("valor"));
-               clientes.add(carro); // Adiciona o objeto Carros à lista de carros
+                        rs.getString("idade"),
+                        rs.getString("sexo"),
+                        rs.getString("rg"),
+                        rs.getString("cpf"));
+                clientes.add(cliente); // Adiciona o objeto clientes à lista de clientes
             }
         } catch (SQLException ex) {
             System.out.println(ex); // Em caso de erro durante a consulta, imprime o erro
@@ -66,27 +69,25 @@ public class ClientesDAO {
 
             // Fecha a conexão, o PreparedStatement e o ResultSet
         }
-        return clientess; // Retorna a lista de carros recuperados do banco de dados
+        return clientes; // Retorna a lista de clientes recuperados do banco de dados
     }
 
     // Cadastrar Carro no banco
-    public void cadastrar(String nome, String modelo, String ano, String placa, String valor) {
+    public void cadastrar(String nome, String idade, String sexo, String rg, String cpf) {
         PreparedStatement stmt = null;
         // Define a instrução SQL parametrizada para cadastrar na tabela
-        String sql = "INSERT INTO carros_lojacarros (nome, modelo, ano, placa, valor) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO clientes_lojacarros2 (nome, idade, sexo, rg, cpf) VALUES (?, ?, ?, ?, ?)";
         try {
             stmt = connection.prepareStatement(sql);
-
             stmt.setString(1, nome);
-            stmt.setString(2, modelo);
-            stmt.setString(3, ano);
-            stmt.setString(4, placa);
-            stmt.setString(5, valor);
+            stmt.setString(2, idade);
+            stmt.setString(3, sexo);
+            stmt.setString(4, rg);
+            stmt.setString(5, cpf);
             stmt.executeUpdate();
             System.out.println("Dados inseridos com sucesso");
-        } catch (
 
-        SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao inserir dados no banco de dados.", e);
         } finally {
             ConnectionFactory.closeConnection(connection, stmt);
@@ -94,18 +95,18 @@ public class ClientesDAO {
     }
 
     // Atualizar dados no banco
-    public void atualizar(String nome, String modelo, String ano, String placa, String valor) {
+    public void atualizar(String nome, String idade, String sexo, String rg, String cpf) {
         PreparedStatement stmt = null;
         // Define a instrução SQL parametrizada para atualizar dados pela placa
-        String sql = "UPDATE carros_lojacarros SET nome = ?, modelo = ?, ano = ?, valor = ? WHERE placa = ?";
+        String sql = "UPDATE clientes_lojacarros2 SET nome = ?, idade = ?, sexo = ?, rg = ?, WHERE cpf = ?";
         try {
             stmt = connection.prepareStatement(sql);
             stmt.setString(1, nome);
-            stmt.setString(2, modelo);
-            stmt.setString(3, ano);
-            stmt.setString(4, valor);
+            stmt.setString(2, idade);
+            stmt.setString(3, sexo);
+            stmt.setString(4, rg);
             // placa é chave primaria não pode ser alterada.
-            stmt.setString(5, placa);
+            stmt.setString(5, cpf);
             stmt.executeUpdate();
             System.out.println("Dados atualizados com sucesso");
         } catch (SQLException e) {
@@ -116,13 +117,13 @@ public class ClientesDAO {
     }
 
     // Apagar dados do banco
-    public void apagar(String placa) {
+    public void apagar(String cpf) {
         PreparedStatement stmt = null;
         // Define a instrução SQL parametrizada para apagar dados pela placa
-        String sql = "DELETE FROM carros_lojacarros WHERE placa = ?";
+        String sql = "DELETE FROM clientes_lojacarros2 WHERE cpf = ?";
         try {
             stmt = connection.prepareStatement(sql);
-            stmt.setString(1, placa);
+            stmt.setString(1, cpf);
             stmt.executeUpdate(); // Executa a instrução SQL
             System.out.println("Dado apagado com sucesso");
         } catch (SQLException e) {
@@ -131,4 +132,5 @@ public class ClientesDAO {
             ConnectionFactory.closeConnection(connection, stmt);
         }
     }
+
 }
--- SituacaoAprendizagem5/guijdbclojacarros/src/main/java/View/ClientesPainel.java
+++ SituacaoAprendizagem5/guijdbclojacarros/src/main/java/View/ClientesPainel.java
@@ -1,143 +1,226 @@
-package View;
+package main.java.View;
 
 import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-import Connection.CarrosDAO;
-import Controller.CarrosControl;
+import main.java.Connection.ClientesDAO;
+import main.java.Controller.ClientesControl;
 
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-
-import Model.Carros;
+import main.java.Model.Clientes;
 
 public class ClientesPainel extends JPanel {
-    // Atributos(componentes)
+    // Atributos
     private JButton cadastrar, apagar, editar;
-    private JTextField carMarcaField, carModeloField, carAnoField, carPlacaField, carValorField;
-    private List<Carros> carros;
+    private JTextField clienteNomeField, clienteIdadeField, clienteSexoField, clienteRgField, clienteCpfField;
+    private List<Clientes> clientes;
     private JTable table;
     private DefaultTableModel tableModel;
     private int linhaSelecionada = -1;
 
-    // Construtor(GUI-JPanel)
+    // Construtor
     public ClientesPainel() {
         super();
         // entrada de dados
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(new JLabel("Cadastro Carros"));
+        add(new JLabel("Cadastro Clientes"));
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new GridLayout(5, 2));
-        inputPanel.add(new JLabel("Marca"));
-        carMarcaField = new JTextField(20);
-        inputPanel.add(carMarcaField);
-        inputPanel.add(new JLabel("Modelo"));
-        carModeloField = new JTextField(20);
-        inputPanel.add(carModeloField);
-        inputPanel.add(new JLabel("Ano"));
-        carAnoField = new JTextField(20);
-        inputPanel.add(carAnoField);
-        inputPanel.add(new JLabel("Placa"));
-        carPlacaField = new JTextField(20);
-        inputPanel.add(carPlacaField);
-        inputPanel.add(new JLabel("Valor"));
-        carValorField = new JTextField(20);
-        inputPanel.add(carValorField);
+
+        inputPanel.add(new JLabel("Nome:"));
+        clienteNomeField = new JTextField(20);
+        inputPanel.add(clienteNomeField);
+
+        inputPanel.add(new JLabel("Idade:"));
+        clienteIdadeField = new JTextField(20);
+        inputPanel.add(clienteIdadeField);
+
+        inputPanel.add(new JLabel("Sexo:"));
+        clienteSexoField = new JTextField(20);
+        inputPanel.add(clienteSexoField);
+
+        inputPanel.add(new JLabel("Rg:"));
+        clienteRgField = new JTextField(20);
+        inputPanel.add(clienteRgField);
+
+        inputPanel.add(new JLabel("Cpf:"));
+        clienteCpfField = new JTextField(20);
+        inputPanel.add(clienteCpfField);
         add(inputPanel);
+
         JPanel botoes = new JPanel();
         botoes.add(cadastrar = new JButton("Cadastrar"));
-        botoes.add(editar = new JButton("Editar"));
-        botoes.add(apagar = new JButton("Apagar"));
+        botoes.add(editar = new JButton("Salvar Edição"));
+        botoes.add(apagar = new JButton("Salvar Exclusão"));
         add(botoes);
         // tabela de carros
         JScrollPane jSPane = new JScrollPane();
         add(jSPane);
         tableModel = new DefaultTableModel(new Object[][] {},
-                new String[] { "Marca", "Modelo", "Ano", "Placa", "Valor" });
+                new String[] { "Nome", "Idade", "Sexo", "Rg", "Cpf" });
         table = new JTable(tableModel);
         jSPane.setViewportView(table);
 
-        // criar o banco de dados
-        new CarrosDAO().criaTabela();
-        // executar o método de atualizar tabela
+        // Criar a tabela se ela não existir
+        new ClientesDAO().criaTabela();
+        // Atualizar a Tabela na Abertura da Janela
         atualizarTabela();
-        // tratamento de eventos(construtor)
-        
-        //tratamento para click do mouse na tabela
+
+        // Tratamento de Eventos (Construtor)
         table.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent evt) {
                 linhaSelecionada = table.rowAtPoint(evt.getPoint());
                 if (linhaSelecionada != -1) {
-                    carMarcaField.setText((String) table.getValueAt(linhaSelecionada, 0));
-                    carModeloField.setText((String) table.getValueAt(linhaSelecionada, 1));
-                    carAnoField.setText((String) table.getValueAt(linhaSelecionada, 2));
-                    carPlacaField.setText((String) table.getValueAt(linhaSelecionada, 3));
-                    carValorField.setText((String) table.getValueAt(linhaSelecionada, 4));
+                    clienteNomeField.setText((String) table.getValueAt(linhaSelecionada, 0));
+                    clienteIdadeField.setText((String) table.getValueAt(linhaSelecionada, 1));
+                    clienteSexoField.setText((String) table.getValueAt(linhaSelecionada, 2));
+                    clienteRgField.setText((String) table.getValueAt(linhaSelecionada, 3));
+                    clienteCpfField.setText((String) table.getValueAt(linhaSelecionada, 4));
                 }
             }
         });
 
-        CarrosControl operacoes = new CarrosControl(carros, tableModel, table);
+        ClientesControl operacoes = new ClientesControl(clientes, tableModel, table);
+        // Configura o metodo "cadastrar" do objeto operacoes com Cpfes dos campos de
+        // entrada
 
-        //tratamento para botão cadastrar
-        cadastrar.addActionListener(e->{
-            operacoes.cadastrar(carMarcaField.getText(), carModeloField.getText(),
-                                carAnoField.getText(), carPlacaField.getText(),
-                                carValorField.getText());
-            carMarcaField.setText("");
-            carModeloField.setText("");
-            carAnoField.setText("");
-            carPlacaField.setText("");
-            carValorField.setText("");
+        cadastrar.addActionListener(e -> {
+
+            String rgText = clienteSexoField.getText();
+            String cpfText = clienteCpfField.getText();
+            String idadeText = clienteIdadeField.getText();
+
+            boolean camposCadastroVazio = clienteNomeField.getText().isEmpty() || clienteIdadeField.getText().isEmpty()
+                    || clienteRgField.getText().isEmpty();
+
+            if ((!rgText.isEmpty() && !cpfText.isEmpty())) {
+                try {
+                    int rg = Integer.parseInt(rgText);
+                    int cpf = Integer.parseInt(cpfText);
+                    int idade = Integer.parseInt(idadeText);
+
+                    if (camposCadastroVazio) {
+                        JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida",
+                                2);
+                    } else if (idade > 18) {
+                        JOptionPane.showMessageDialog(null, "É Necessário ter 18 Anos para Registrar o Cadastro", "Informação Inválida",
+                                2);
+                    } else {
+                        operacoes.cadastrar(clienteNomeField.getText(), clienteIdadeField.getText(), clienteSexoField.getText(),
+                                clienteRgField.getText(), clienteCpfField.getText());
+
+                        // Limpa os campos de entrada após a operação de exclusão
+                        clienteNomeField.setText("");
+                        clienteIdadeField.setText("");
+                        clienteSexoField.setText("");
+                        clienteRgField.setText("");
+                        clienteCpfField.setText("");
+                    }
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida", 2);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida", 2);
+            }
         });
 
-        //tratamento do botão editar
-        editar.addActionListener(e->{
-            operacoes.atualizar(carMarcaField.getText(), carModeloField.getText(),
-                                carAnoField.getText(), carPlacaField.getText(),
-                                carValorField.getText());
-            carMarcaField.setText("");
-            carModeloField.setText("");
-            carAnoField.setText("");
-            carPlacaField.setText("");
-            carValorField.setText("");
+        // Configura a ação do botão editar
+        editar.addActionListener(e -> {
+            // Chama o metodo "editar" do objeto operacoes com os Cpfes dos campos de
+            // entrada
+            String rgText = clienteRgField.getText();
+            String cpfText = clienteCpfField.getText();
+            String idadeText = clienteIdadeField.getText();
+
+            boolean camposCadastroVazio = clienteNomeField.getText().isEmpty() || clienteIdadeField.getText().isEmpty()
+                    || clienteRgField.getText().isEmpty();
+
+            if ((!rgText.isEmpty() && !cpfText.isEmpty())) {
+                try {
+                    int rg = Integer.parseInt(rgText);
+                    int cpf = Integer.parseInt(cpfText);
+                    int idade = Integer.parseInt(idadeText);
+
+                    if (camposCadastroVazio) {
+                        JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida",
+                                2);
+                    } else if (idade > 18) {
+                        JOptionPane.showMessageDialog(null, "É Necessário ter 18 Anos para Registrar o Cadastro", "Informação Inválida",
+                                2);
+                    }
+                    
+                    else {
+                        operacoes.atualizar(clienteNomeField.getText(), clienteIdadeField.getText(), clienteSexoField.getText(),
+                                clienteRgField.getText(), clienteCpfField.getText());
+                        // Limpa os campos de entrada após a operação de edição
+                        clienteNomeField.setText("");
+                        clienteIdadeField.setText("");
+                        clienteSexoField.setText("");
+                        clienteRgField.setText("");
+                        clienteCpfField.setText("");
+                    }
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida", 2);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Preencha os Campos Corretamente", "Informação Inválida", 2);
+            }
+
         });
 
-        //tratamento do botão apagar
-        apagar.addActionListener(e->{
-            operacoes.apagar(carPlacaField.getText());
-            carMarcaField.setText("");
-            carModeloField.setText("");
-            carAnoField.setText("");
-            carPlacaField.setText("");
-            carValorField.setText("");
+        // Configura a ação do botão "apagar" para excluir um registro no banco de dados
+        apagar.addActionListener(e -> {
+
+            // Chama o método "apagar" do objeto operacoes com o Cpf do campo de entrada
+            // "Rg"
+
+            boolean camposCadastroVazio = clienteNomeField.getText().isEmpty() || clienteIdadeField.getText().isEmpty()
+                    || clienteRgField.getText().isEmpty();
+
+            if (camposCadastroVazio) {
+                JOptionPane.showMessageDialog(null, "Não é possível dados vazios", "Informação Inválida",
+                        2);
+            } else if (JOptionPane.showConfirmDialog(null, "Deseja excluir esse carro?",
+                    "Excluindo Tarefa...", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+
+                operacoes.apagar(clienteRgField.getText());
+
+                // Limpa os campos de entrada após a operação de exclusão
+                clienteNomeField.setText("");
+                clienteIdadeField.setText("");
+                clienteSexoField.setText("");
+                clienteRgField.setText("");
+                clienteCpfField.setText("");
+
+            }
         });
 
+    };
 
-    }
-
-    // métodos (atualizar tabela)
+    // Métodos (Atualizar Tabela)
     // Método para atualizar a tabela de exibição com dados do banco de dados
     private void atualizarTabela() {
         tableModel.setRowCount(0); // Limpa todas as linhas existentes na tabela
-        carros = new CarrosDAO().listarTodos();
-        // Obtém os carros atualizados do banco de dados
-        for (Carros carro : carros) {
-            // Adiciona os dados de cada carro como uma nova linha na tabela Swing
-            tableModel.addRow(new Object[] { carro.getMarca(), carro.getModelo(),
+        clientes = new ClientesDAO().listarTodos();
+        // Obtém os Clientes atualizados do banco de dados
+        for (Clientes cliente : clientes) {
+            // Adiciona os dados de cada cliente como uma nova linha na tabela Swing
+            tableModel.addRow(new Object[] { cliente.getNome(), cliente.getIdade(),
 
-                    carro.getAno(), carro.getPlaca(), carro.getValor() });
+                    cliente.getSexo(), cliente.getRg(), cliente.getCpf() });
         }
     }
 }
